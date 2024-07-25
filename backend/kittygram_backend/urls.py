1b47a25c6f54053a3222d0f76a36a1c429b558aa
Albina Giliazova <giliazova@mail.ru>
2024-07-25 12:58:08 +0500

    fix on review

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,19 +1,24 @@
+from django.conf import settings  # type: ignore
+from django.conf.urls.static import static  # type: ignore
+from django.contrib import admin  # type: ignore
+from django.urls import include, path  # type: ignore
+from rest_framework import routers  # type: ignore
+
 from cats.views import AchievementViewSet, CatViewSet
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
-urlpatterns = [
+api_urlpatterns: list[path] = [
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),  # Работа с пользователями
+    path('', include('djoser.urls.authtoken')),  # Работа с токенами
+]
+
+urlpatterns: list[path] = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),  # Работа с пользователями
-    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path('api/', include(api_urlpatterns)),
 ]
 
 if settings.DEBUG:
