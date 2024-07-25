3d3041dd60f76fe75cc99bce874f58796b39a6ae
Albina Giliazova <giliazova@mail.ru>
2024-07-25 13:34:51 +0500

    fix

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,10 +1,9 @@
+from cats.views import AchievementViewSet, CatViewSet
 from django.conf import settings  # type: ignore
 from django.conf.urls.static import static  # type: ignore
 from django.contrib import admin  # type: ignore
 from django.urls import include, path  # type: ignore
 from rest_framework import routers  # type: ignore
-
-from cats.views import AchievementViewSet, CatViewSet
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
